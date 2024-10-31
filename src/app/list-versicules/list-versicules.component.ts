68fe2f4b0a90a5a40c1e839b5c9749f326148064
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-31 11:39:11 -0300

    service get it's working and project it's good

--- src/app/list-versicules/list-versicules.component.ts
+++ src/app/list-versicules/list-versicules.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TextBibleService } from '../text-bible.service';
+import { Versicule } from '../Versicule';
 
 @Component({
   selector: 'app-list-versicules',
   templateUrl: './list-versicules.component.html',
   styleUrl: './list-versicules.component.css'
 })
-export class ListVersiculesComponent {
+export class ListVersiculesComponent implements OnInit {
+
+  Verses: Versicule [] = [];
+
+  constructor(private service:TextBibleService) { }
+
+  ngOnInit(): void {
+    this.LoadVerses()
+  }
+
+  LoadVerses(){
+    this.service.getTextHolyBible().subscribe({
+      next: data => this.Verses = data
+    })
+  }
+
 
 }
