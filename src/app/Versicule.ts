427c325fd30e859174dc0a9c6b6f8aca56f1fc21
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-31 10:43:10 -0300

    Create a interface, and alterations in css component

---
+++ src/app/Versicule.ts
@@ -0,0 +1,8 @@
+export interface Versicule {
+    id: number,
+    text: string,
+    book: string,
+    chapter: number,
+    verse: number,
+    testament: string
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListVersiculesComponent } from './list-versicules/list-versicules.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Versicules', component: ListVersiculesComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
